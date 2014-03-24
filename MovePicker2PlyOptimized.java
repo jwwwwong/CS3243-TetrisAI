50709afcde041e939a2330dd038eb7dcaed18fcc
John Wong <johnwong@invite.com.sg>
2014-03-24 22:04:02 +0800

    added MovePicker2PlyOptimized class

---
+++ MovePicker2PlyOptimized.java
@@ -0,0 +1,6 @@
+public class MovePicker2PlyOptimized extends MovePicker {
+    @Override
+    public int pickMove(TetrisState currentState, int[][] legalMoves, UtilityCalculator utilityCalculator)  {
+        return 0;
+    }
+}
