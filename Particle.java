d26d691c02dc3bd4c95f6b665de1c28943ce942e
John Wong <johnwong@invite.com.sg>
2014-03-31 12:55:36 +0800

    updated particle swarm class

--- Particle.java
+++ Particle.java
@@ -1,16 +1,12 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Random;
 
 
 public class Particle implements Runnable{
-     private double [] curPosition;
+     private final double [] curPosition;
      private int particleBestFitness;
-     private double[] curVelocity;
+     private final double[] curVelocity;
      
-     private int particleID;
+     private final int particleID;
      private double[] particleBestPosition;
      
      private double[] globalBestPostion;
@@ -20,15 +16,13 @@ public class Particle implements Runnable{
      private boolean firstRun;
      
      AiEvaluator evaluator;
-     private int dimension;
+     private final int dimension;
      MovePicker movePicker;
      UtilityCalculator utilityCalculator;
       
      
     public Particle(int particleID,int dimension, double maxSearchRange, double minSearchRange) {
-		// TODO Auto-generated constructor stub
-    	
-    	this.dimension=dimension;
+		this.dimension=dimension;
     	this.particleBestFitness=0;
     	firstRun=true;
     	this.particleID=particleID;
@@ -36,111 +30,37 @@ public class Particle implements Runnable{
     	curVelocity= new double[dimension];
     	particleBestPosition= new double [dimension];
     	
-    	for(int i=0; i<dimension; i++)
-    	{
+    	for(int i = 0; i < dimension; i++) {
     		curPosition[i]=minSearchRange+new Random().nextDouble()*(maxSearchRange-minSearchRange);
     	}
     	double rangeLength=maxSearchRange-minSearchRange;
-    	for(int i=0; i<dimension; i++)
-    	{
+    	for(int  i =0; i < dimension; i++) {
     		curVelocity[i]=-rangeLength+ new Random().nextDouble()*(rangeLength*2);
     	}
     	particleBestPosition=ArrayHandler.makeCopy(curPosition);
     	
     	evaluator = new RandomSequenceEvaluator();
-		 utilityCalculator = new UtilityCalculator();
-	      movePicker = new MovePicker1Ply();
-    	
-    	
-    	
+		utilityCalculator = new UtilityCalculator();
+	    movePicker = new MovePicker1Ply();    	
 	} 
     
-    
-
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		  
+        if(!firstRun) {
+            for(int i=0; i<dimension; i++) {
+                curVelocity[i]=INERTIA_WEIGHT*curVelocity[i]+PERSONAL_WEIGHT*new Random().nextDouble()*(particleBestPosition[i]-curPosition[i])+GLOBAL_WEIGHT*new Random().nextDouble()*(globalBestPostion[i]-curPosition[i]);
+                curPosition[i]+=curVelocity[i];
+            }	    	
+        }
 	      
-	      if(!firstRun)
-	      {
-	    	  for(int i=0; i<dimension; i++)
-	    	  {   //update velocity
-	    		  curVelocity[i]=INERTIA_WEIGHT*curVelocity[i]+PERSONAL_WEIGHT*new Random().nextDouble()*(particleBestPosition[i]-curPosition[i])+GLOBAL_WEIGHT*new Random().nextDouble()*(globalBestPostion[i]-curPosition[i]);
-	    		  //update position
-	    		  curPosition[i]+=curVelocity[i];
-	    	  }
-	    	
-	      }
-//	        File file = new File("C:/Users/A0081205/Dropbox/CS3243 workplace/good.txt");
-//	      
-//	    	if (!file.exists()) {
-//				try {
-//					file.createNewFile();
-//				} catch (IOException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//			}
-// 
-//			
-//	        
-//	        String string="current parameters:";
-//			for(int j=0; j<dimension; j++)
-//			{
-//				string+=curPosition[j];
-//				if(j!=dimension-1)
-//				{
-//					string+=",";
-//				}
-//				else
-//				{
-//					string+="\n";
-//				}
-//					
-//			}
-//			System.out.println(string);
-			
-//			FileWriter fw;
-//			try {
-//				fw = new FileWriter(file.getAbsoluteFile());
-//				BufferedWriter bw = new BufferedWriter(fw);
-//				bw.write(string);
-//				bw.close();
-//			} catch (IOException e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
-//			
-//		
-// 
-//			System.out.println("Done");
-			
-			
+	    utilityCalculator.setWeights(curPosition);	    
+	    int performance = evaluator.evaluateAveragePerformance(movePicker, utilityCalculator);	    
+        if(performance>particleBestFitness) {
+            particleBestFitness=performance;
+            particleBestPosition=ArrayHandler.makeCopy(curPosition);
+        }
 	      
-	      utilityCalculator.setWeights(curPosition);
-	    
-	  //   System.out.println("Particle "+particleID+" still running");
-	     
-	     int performance = evaluator.evaluateAveragePerformance(movePicker, utilityCalculator);
-	    
-	  //   System.out.println("Particle "+particleID+" stop running");
-	      
-	      if(performance>particleBestFitness)
-	      {
-	    	  particleBestFitness=performance;
-	    	  particleBestPosition=ArrayHandler.makeCopy(curPosition);
-	      }
-	      
-	      
-	      firstRun=false;  
-	      
-	      
-	      
-	      
-	      
-	      
-		
+        firstRun=false;  	    
 	}
 	
 	public int getParticleBestFitness() {
@@ -155,12 +75,10 @@ public class Particle implements Runnable{
 	
      @Override
     public String toString() {
-		
-    	// TODO Auto-generated method stub
-    	 String string="";
-    	 string+="Particle "+ particleID+" : "+ "Personal Best: "+ particleBestFitness+"\n";
+        String string="";
+    	string+="Particle "+ particleID+" : "+ "Personal Best: "+ particleBestFitness+"\n";
     	 
-    	 return string;
+    	return string;
     }
 
 }
--- ParticleSwarmOptimizer.java
+++ ParticleSwarmOptimizer.java
@@ -1,11 +1,11 @@
 
 public class ParticleSwarmOptimizer extends Optimizer {
 
-	final static int MAX_ITERATION=100000;
-	final static int NUM_PARTICLE=8;
-	final static double MIN_SEARCH_RANGE=-100;
-	final static double MAX_SEARCH_RANGE=100;
-	private int globalBestFitness=-1;
+	final static int MAX_ITERATION = 100000;
+	final static int NUM_PARTICLE = 8;
+	final static double MIN_SEARCH_RANGE = -100;
+	final static double MAX_SEARCH_RANGE = 100;
+	private int globalBestFitness = -1;
 	private double [] globalBestPosition;
 	private Particle[] particle;
 
@@ -13,78 +13,50 @@ public class ParticleSwarmOptimizer extends Optimizer {
 	@Override
 	public void optimize(UtilityCalculator utilityCalculator,
 			MovePicker movePicker, AiEvaluator evaluator) {
-		// TODO Auto-generated method stub
 		int dimension= utilityCalculator.getWeights().length;
-		globalBestPosition= new double[dimension];
-		globalBestFitness=0;
+		globalBestPosition = new double[dimension];
+		globalBestFitness = 0;
 		particle = new Particle[NUM_PARTICLE];
-		for(int i=0; i<NUM_PARTICLE; i++)
-		{
+		for(int i = 0; i < NUM_PARTICLE; i++) {
 			particle[i]= new Particle(i, dimension, MAX_SEARCH_RANGE, MIN_SEARCH_RANGE);
 		}
 
 		Thread [] particleThread =new Thread[NUM_PARTICLE];
-		for(int i=0; i<MAX_ITERATION; i++)
-		{
-
-			for(int j=0; j<NUM_PARTICLE; j++)
-			{
+		for(int i = 0; i < MAX_ITERATION; i++) {
+			for(int j=0; j<NUM_PARTICLE; j++) {
 				particleThread[j]= new Thread(particle[j]);
 				particleThread[j].run();
-				//				particle[j].run();
 			}
 
-
-			for(int j=0; j<NUM_PARTICLE; j++)
-			{
-				{
-					try {
-						particleThread[j].join();
-
-
-						int personalBest= particle[j].getParticleBestFitness();
-						if(personalBest>globalBestFitness)
-						{
-							globalBestFitness=personalBest;
-							globalBestPosition=ArrayHandler.makeCopy(particle[j].getParticleBestPosition());
-
-						}
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				} 
-
-
+			for(int j = 0; j < NUM_PARTICLE; j++) {				
+                try {
+                    particleThread[j].join();
+                    int personalBest = particle[j].getParticleBestFitness();
+                    if(personalBest>globalBestFitness) {
+                        globalBestFitness=personalBest;
+                        globalBestPosition=ArrayHandler.makeCopy(particle[j].getParticleBestPosition());
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 			}
 
-			for(int j=0; j<NUM_PARTICLE; j++)
-			{
+			for(int j = 0; j < NUM_PARTICLE; j++) {
 				particle[j].setGlobalBestPostion(globalBestPosition);
 			}
 			System.out.println("Global best: " +globalBestFitness);
 			System.out.println("Global best parameters:");
-			for(int j=0; j<dimension; j++)
-			{
+			for(int j=0; j<dimension; j++) {
 				System.out.print(globalBestPosition[j]);
-				if(j!=dimension-1)
-				{
+				if(j!=dimension-1) {
 					System.out.print(",");
 				}
-				else
-				{
+                else {
 					System.out.println();
 				}
-
 			}
-
 		}
-
 	}
-
-
-
 }
 
 
